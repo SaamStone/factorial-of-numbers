7f399d0cdd64ffd01772617b6faccb0739505dac
SaamStone <Sairam47003@gmail.com>
2025-08-15 23:15:36 +0530

    The code is UPDATED to find weather the output is even number is Even or Odd Number

--- factorial.py
+++ factorial.py
@@ -2,6 +2,10 @@ def factorials(num):
     result = 1
     for i in range(1,num+1):
         result*=i
+    if result % 2 == 0:
+            print("its a even number")
+    else:
+            print("its a odd number")
     return result
 x = factorials(int(input("Enter the Number : ")))
 print(x)
